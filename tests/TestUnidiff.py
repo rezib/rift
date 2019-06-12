rezib/rift

commit af7afb0602a893b53fb542bd178cbf2c71891f17

Controller: test if patch is binary and not empty
If patch file contains binary data is not parsed by unidiff.
We fixed unidiff to get binary files and be able to check its content.

A binary patch looks like :
diff --git /dev/null b/packages/pkg/sources/pkg-1.0-1.tar.gz
index
6cd0ff6ec591f7f51a3479d7b66c6951a2b4afa9..91ef2076b67f3158ec1670fa7b88d88b2816aa91
100644
GIT binary patch
literal 8
PcmZQ%;Sf+z_{#tQ1BL-x

literal 4
LcmZQ%;Sc}-05kv|

or

diff --git a/sources/a.tar.gz b/sources/a.tar.gz
index fcd49dd..91ef207 100644
Binary files a/sources/a.tar.gz and b/sources/a.tar.gz differ

Change-Id: I46ba3e6c9bc3786ef76323dea0e7ac38c79aa365

lib/unidiff.py
@@ -30,8 +30,7 @@ LINE_TYPE_ADD = '+'
 LINE_TYPE_DELETE = '-'
 LINE_TYPE_CONTEXT = ' '
 
-RE_SOURCE_FILENAME = re.compile(r'^--- (?P<filename>[^\t\n]+)')
-RE_TARGET_FILENAME = re.compile(r'^\+\+\+ (?P<filename>[^\t\n]+)')
+RE_DIFF_PATCH = re.compile(r'^diff --git (?P<source>[^\t]+) (?P<target>[^\t]+)')
 
 # @@ (source offset, length) (target offset, length) @@ (section header)
 RE_HUNK_HEADER = re.compile(
@@ -265,19 +264,15 @@ def parse_unidiff(diff):
     """Unified diff parser, takes a file-like object as argument."""
     ret = PatchSet()
     current_patch = None
+    source_file = None
+    target_file = None
 
     for line in diff:
-        # check for source file header
-        check_source = RE_SOURCE_FILENAME.match(line)
-        if check_source:
-            source_file = check_source.group('filename')
-            current_patch = None
-            continue
-
-        # check for target file header
-        check_target = RE_TARGET_FILENAME.match(line)
-        if check_target:
-            target_file = check_target.group('filename')
+        ## check for source file header
+        check_binary = RE_DIFF_PATCH.match(line)
+        if check_binary and not source_file:
+            source_file = check_binary.group('source')
+            target_file = check_binary.group('target')
             current_patch = PatchedFile(source_file, target_file)
             ret.append(current_patch)
             continue

lib/rift/Controller.py
@@ -705,7 +705,12 @@ def action(config, args):
     elif args.command == 'validdiff':
 
         pkglist = {}
-        for patchedfile in parse_unidiff(args.patch):
+        patchedfiles = parse_unidiff(args.patch)
+
+        if not patchedfiles:
+            raise RiftError("Invalid patch detected (empty commit ?)")
+
+        for patchedfile in patchedfiles:
 
             filepath = patchedfile.path
             names = filepath.split(os.path.sep)
@@ -758,7 +763,8 @@ def action(config, args):
 
                 # sources/
                 elif fullpath.startswith(pkg.sourcesdir) and len(names) == 2:
-                    # XXX: Check binary/no binary
+                    if not len(patchedfile):
+                        raise RiftError("Binary file detected: %s" % filepath)
                     logging.debug('Ignoring source file: %s', names[1])
 
                 # tests/

tests/Controller.py
@@ -7,6 +7,7 @@ import os.path
 from TestUtils import make_temp_file, make_temp_dir, RiftTestCase
 
 from rift.Controller import Config, main
+from rift import RiftError
 
 
 class ControllerTest(RiftTestCase):
@@ -115,7 +116,7 @@ class ControllerProjectTest(RiftTestCase):
         srcdir = os.path.join(self.pkgdirs[name], 'sources')
         os.mkdir(srcdir)
 
-        # ./packages/pkg/sources/pkg-version-release.tar.gz
+        # ./packages/pkg/sources/pkg-version.tar.gz
         self.pkgsrc[name] = os.path.join(srcdir,
                                          "{0}-{1}.tar.gz".format(name, version))
         with open(self.pkgsrc[name], "w") as src:
@@ -158,3 +159,49 @@ index 0000000..e845566
             patch = make_temp_file(patch_template.format(filename))
             self.assertEqual(main(['validdiff', patch.name]), 0)
 
+    def test_validdiff_binary(self):
+        """ Should fail if source file is a binary file """
+        pkgname = 'pkg'
+        pkgvers = 1.0
+        self.make_pkg(name=pkgname, version=pkgvers)
+        pkgsrc = os.path.join('packages', 'pkgname', 'sources',
+                              '{0}-{1}.tar.gz'.format(pkgname, pkgvers))
+        patch = make_temp_file("""
+commit 0ac8155e2655321ceb28bbf716ff66d1a9e30f29 (HEAD -> master)
+Author: Myself <buddy@somewhere.org>
+Date:   Thu Apr 25 14:30:41 2019 +0200
+
+    packages: update 'pkg' sources
+
+diff --git /dev/null b/{0}
+index fcd49dd..91ef207 100644
+Binary files a/sources/a.tar.gz and b/sources/a.tar.gz differ
+""".format(pkgsrc))
+        self.assert_except(RiftError, "Binary file detected: {0}\n".format(pkgsrc),
+                           main, ['validdiff', patch.name])
+
+    def test_validdiff_binary_with_content(self):
+        """ Should fail if source file is a binary file (diff --binary) """
+        pkgname = 'pkg'
+        pkgvers = 1.0
+        self.make_pkg(name=pkgname, version=pkgvers)
+        pkgsrc = os.path.join('packages', 'pkgname', 'sources',
+                              '{0}-{1}.tar.gz'.format(pkgname, pkgvers))
+        patch = make_temp_file("""
+commit 0ac8155e2655321ceb28bbf716ff66d1a9e30f29 (HEAD -> master)
+Author: Myself <buddy@somewhere.org>
+Date:   Thu Apr 25 14:30:41 2019 +0200
+
+    packages: update 'pkg' sources
+
+diff --git /dev/null b/{0}
+index 6cd0ff6ec591f7f51a3479d7b66c6951a2b4afa9..91ef2076b67f3158ec1670fa7b88d88b2816aa91 100644
+GIT binary patch
+literal 8
+PcmZQ%;Sf+z_{{#tQ1BL-x
+
+literal 4
+LcmZQ%;Sc}}-05kv|
+""".format(pkgsrc))
+        self.assert_except(RiftError, "Binary file detected: {0}\n".format(pkgsrc),
+                           main, ['validdiff', patch.name])

commit 1c1fc78617108476b53b73c87d87606e159427ee

unidiff: parse all files from a patch
Commit af7afb0602a893b53fb542bd178cbf2c71891f17 introduced a regression
when parsing patches. Only the first file was parsed.
This patch fixes this bug and add tests for this usecase.

Change-Id: I434fbe69bf10df97e455adf7eeb90b2457ab4000

lib/unidiff.py
@@ -269,10 +269,10 @@ def parse_unidiff(diff):
 
     for line in diff:
         ## check for source file header
-        check_binary = RE_DIFF_PATCH.match(line)
-        if check_binary and not source_file:
-            source_file = check_binary.group('source')
-            target_file = check_binary.group('target')
+        check_source = RE_DIFF_PATCH.match(line)
+        if check_source:
+            source_file = check_source.group('source')
+            target_file = check_source.group('target')
             current_patch = PatchedFile(source_file, target_file)
             ret.append(current_patch)
             continue

tests/TestUnidiff.py
@@ -1,0 +1,50 @@
+from unidiff import parse_unidiff
+from TestUtils import make_temp_file, make_temp_dir, RiftTestCase
+
+class UnidiffTest(RiftTestCase):
+    """
+    Test class for unidiff
+    """
+
+    def testMultiFilePatch(self):
+        """ Test if unidiff parse correctly a patch with mutliple files """
+        unifiedpatch = make_temp_file("""
+commit 0ac8155e2655321ceb28bbf716ff66d1a9e30f29 (HEAD -> master)
+Author: Myself <buddy@somewhere.org>
+Date:   Thu Apr 25 14:30:41 2019 +0200
+
+    packages: add 'foo'
+
+diff --git a/file1 b/file1
+new file mode 100644
+index 0000000..32ac08e
+--- /dev/null
++++ b/file1
+@@ -0,0 +1 @@
++foo
+diff --git a/file2 b/file2
+new file mode 100644
+index 0000000..257cc56
+--- /dev/null
++++ b/file2
+@@ -0,0 +1 @@
++bar
+diff --git a/file3 b/file3
+new file mode 100644
+index 0000000..257cc56
+--- /dev/null
++++ b/file3
+@@ -0,0 +1 @@
++pub
+""")
+
+
+        with open(unifiedpatch.name, 'r') as f:
+            patchedfiles = parse_unidiff(f)
+        self.assertEqual(len(patchedfiles), 3)
+        filenames = []
+        for patchedfile in patchedfiles:
+            filenames.append(patchedfile.path)
+        self.assertTrue("file1" in filenames)
+        self.assertTrue("file2" in filenames)
+        self.assertTrue("file3" in filenames)
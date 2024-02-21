347a300610a5b318d5ab3ba96e3eb3abed33db1e
Rémi Palancher <remi@rackslab.io>
2024-02-21 09:52:34 +0100

    Tests: generate valid Mock config
    Generate a valid mock configuration in Rift project directory with
    RiftProjectTestCase so that Rift can execute Mock for tests purposes.
    
    Change-Id: Ia04a4d35ee0c9e7ff88b7b2c78f531373a2b53a3

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -12,6 +12,55 @@ import unittest
 import os
 
 from rift.Config import Config, Staff, Modules
+from rift.Mock import Mock
+
+MOCK_CONF = '''\
+config_opts.setdefault('plugin_conf', {})
+config_opts['plugin_conf']['ccache_enable'] = False
+config_opts['root'] = '{{ name }}'
+config_opts['target_arch'] = '{{ arch }}'
+config_opts['legal_host_arches'] = ('{{ arch }}',)
+config_opts['chroot_setup_cmd'] = 'install centos-release @base @development'
+config_opts['dist'] = 'el8'
+config_opts['releasever'] = '8'
+config_opts['priorities.conf'] = "[main]\\nenabled = 1\\n"
+config_opts['package_manager'] = 'dnf'
+config_opts['bootstrap_image'] = 'centos:8'
+config_opts['isolation'] = 'simple'
+config_opts['chroot_setup_cmd'] = (
+    'install tar gcc-c++ redhat-rpm-config redhat-release which xz sed make '
+    'bzip2 gzip gcc coreutils unzip shadow-utils diffutils cpio bash gawk '
+    'rpm-build info patch util-linux findutils grep autoconf automake libtool '
+    'binutils bison flex gdb glibc-devel pkgconf pkgconf-m4 pkgconf-pkg-config '
+    'rpm-sign byacc ctags diffstat intltool patchutils pesign source-highlight '
+    'cmake rpmdevtools rpmlint libtirpc-devel kernel-rpm-macros'
+)
+config_opts['yum.conf'] = """
+[main]
+cachedir=/var/cache/yum
+debuglevel=1
+reposdir=/dev/null
+logfile=/var/log/yum.log
+retries=20
+obsoletes=1
+gpgcheck=0
+assumeyes=1
+syslog_ident=mock
+syslog_device=
+plugins=1
+best=False
+
+{% for repo in repos %}
+[{{ repo.name }}]
+name={{ repo.name }}
+baseurl={{ repo.url }}
+priority={{ repo.priority }}
+{%if repo.module_hotfixes %}
+module_hotfixes={{ repo.module_hotfixes }}
+{% endif %}
+{% endfor %}
+"""
+'''
 
 
 class RiftTestCase(unittest.TestCase):
@@ -75,12 +124,17 @@ class RiftProjectTestCase(RiftTestCase):
         self.staff.load(self.staffpath)
         self.modules = Modules(config=self.config, staff=self.staff)
         self.modules.load(self.modulespath)
+        # ./mock.tpl
+        self.mocktpl = os.path.join(self.projdir, Mock.MOCK_TEMPLATE)
+        with open(self.mocktpl, "w") as fh:
+            fh.write(MOCK_CONF)
 
     def tearDown(self):
         os.chdir(self.cwd)
         os.unlink(self.projectconf)
         os.unlink(self.staffpath)
         os.unlink(self.modulespath)
+        os.unlink(self.mocktpl)
         os.rmdir(self.annexdir)
         for spec in self.pkgspecs.values():
             os.unlink(spec)

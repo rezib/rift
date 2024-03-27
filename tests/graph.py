eda6a4ec48f3b46c240eb7a1b65e1caffa583e42
Rémi Palancher <remi@rackslab.io>
2024-03-27 14:19:15 +0100

    graph: auto rebuild/validation of reverse deps
    This commit introduces the graph modules with class to handle a graph of
    reverse dependencies between packages in a Rift project.
    
    When a package is built, especially when its version has changed, it is
    important to verify that it does not break build and tests of other
    packages that depends on it (aka. reverse dependencies).
    
    With this new feature, as soon as dependency_tracking option is enabled
    in project configuration, the graph of reverse dependencies is
    automatically created with rift build and validate actions, unless
    --skip-deps option is set on command line. Dependencies are either found
    in packages info.yaml metadata file or by parsing subpackages (including
    their provides) and build requirements out of RPM spec files as a
    fallback.
    
    Rift then solves the reverse dependencies of the packages provided by
    users with build and validate actions to determine the packages that
    should be rebuilt or validated consequently.
    
    Unit tests are added to validate the code introduced for this feature.

---
+++ tests/graph.py
@@ -0,0 +1,350 @@
+#
+# Copyright (C) 2020 CEA
+#
+import os
+import shutil
+
+from rift.graph import PackagesDependencyGraph
+from rift.Package import Package
+from TestUtils import RiftProjectTestCase, SubPackage
+
+class GraphTest(RiftProjectTestCase):
+    """
+    Tests class for PackageDependencyGraph
+    """
+    def test_one_package(self):
+        """ Test graph with one package """
+        pkg_name = 'fake'
+        self.make_pkg(name=pkg_name)
+        package = Package(pkg_name, self.config, self.staff, self.modules)
+        graph = PackagesDependencyGraph.from_project(
+            self.config,
+            self.staff,
+            self.modules
+        )
+        self.assertEqual(len(graph.nodes), 1)
+        build_requirements = graph.solve(package)
+        self.assertEqual(len(build_requirements), 1)
+        self.assertEqual(build_requirements[0].package.name, package.name)
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+
+    def test_packages_unable_load(self):
+        """ Test graph build with package unable to load """
+        pkgs_names = [ 'success', 'failed']
+        packages = {}
+        for pkg_name in pkgs_names:
+            self.make_pkg(name=pkg_name)
+            packages[pkg_name] = Package(pkg_name, self.config, self.staff, self.modules)
+        # Remove info.yaml in packages failed to generate error
+        os.unlink(packages['failed'].metafile)
+        # Build packages graph
+        with self.assertLogs(level='WARNING') as cm:
+            graph = PackagesDependencyGraph.from_project(
+                self.config,
+                self.staff,
+                self.modules
+            )
+        # Check warning message has been emitted
+        self.assertEqual(
+            cm.output,
+            [ "WARNING:root:Skipping package failed unable to load: [Errno 2] "
+              "No such file or directory: "
+              f"'{self.projdir}/packages/failed/info.yaml'" ]
+        )
+        # Check success package is successfully loaded anyway.
+        self.assertEqual(len(graph.nodes), 1)
+        self.assertEqual(graph.nodes[0].package.name, 'success')
+
+    def test_dump(self):
+        """ Test graph dump """
+        pkg_name = 'fake'
+        self.make_pkg(name=pkg_name)
+        package = Package(pkg_name, self.config, self.staff, self.modules)
+        graph = PackagesDependencyGraph.from_project(
+            self.config,
+            self.staff,
+            self.modules
+        )
+        with self.assertLogs(level='INFO') as cm:
+            graph.dump()
+        self.assertEqual(
+            cm.output,
+            [
+                'INFO:root:→ fake',
+                "INFO:root:  requires: ['br-package']",
+                "INFO:root:  subpackages: ['fake', 'fake-provide']",
+                'INFO:root:  rdeps: []'
+            ]
+        )
+
+    def test_empty_solve(self):
+        """ Test solve with package not in graph """
+        pkg_name = 'one'
+        self.make_pkg(name=pkg_name)
+        graph = PackagesDependencyGraph.from_project(
+            self.config,
+            self.staff,
+            self.modules
+        )
+        package = Package('another', self.config, self.staff, self.modules)
+        build_requirements = graph.solve(package)
+        self.assertEqual(len(build_requirements), 0)
+
+    def test_multiple_packages(self):
+        """ Test graph with multiple packages and dependencies in info.yaml """
+        # Define 3 packages with depends in info.yaml, in both string and list
+        # formats.
+        self.make_pkg(
+            name='libone',
+            metadata={
+                'depends': 'libtwo'
+            }
+        )
+        self.make_pkg(
+            name='libtwo',
+        )
+        self.make_pkg(
+            name='my-software',
+            metadata={
+                'depends': ['libone']
+            }
+        )
+
+        # Load graph
+        graph = PackagesDependencyGraph.from_project(
+            self.config,
+            self.staff,
+            self.modules
+        )
+        self.assertEqual(len(graph.nodes), 3)
+
+        # Rebuild of my-software does not trigger rebuild of other packages.
+        build_requirements = graph.solve(
+            Package('my-software', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 1)
+        self.assertEqual(build_requirements[0].package.name, 'my-software')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+
+        # Rebuild of libone triggers rebuild of my-software because it depends
+        # on libone.
+        build_requirements = graph.solve(
+            Package('libone', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 2)
+        self.assertEqual(build_requirements[0].package.name, 'libone')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+        self.assertEqual(build_requirements[1].package.name, 'my-software')
+        self.assertEqual(
+            build_requirements[1].reasons,
+            ["depends on libone"],
+        )
+
+        # Rebuild of libtwo triggers rebuild of:
+        # - libone because it depends on libtwo
+        # - my-software because it depends on libone
+        build_requirements = graph.solve(
+            Package('libtwo', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 3)
+        self.assertEqual(build_requirements[0].package.name, 'libtwo')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+        self.assertEqual(build_requirements[1].package.name, 'libone')
+        self.assertEqual(
+            build_requirements[1].reasons,
+            ["depends on libtwo"],
+        )
+        self.assertEqual(build_requirements[2].package.name, 'my-software')
+        self.assertEqual(
+            build_requirements[2].reasons,
+            ["depends on libone"],
+        )
+
+    def test_multiple_packages_spec_fallback(self):
+        """ Test graph with multiple packages and dependencies in RPM spec files """
+        # Define 3 packages without depends in info.yaml but with build requires
+        # on others subpackages.
+        self.make_pkg(
+            name='libone',
+            build_requires=['libtwo-devel >= 3.5'],
+            subpackages=[
+                SubPackage('libone-bin'),
+                SubPackage('libone-devel')
+            ]
+        )
+        self.make_pkg(
+            name='libtwo',
+            subpackages=[
+                SubPackage('libtwo-bin'),
+                SubPackage('libtwo-devel')
+            ]
+        )
+        self.make_pkg(
+            name='my-software',
+            build_requires=['libone-devel = 3, libtwo-devel'],
+        )
+
+        def load_graph():
+            graph = PackagesDependencyGraph.from_project(
+                self.config,
+                self.staff,
+                self.modules
+            )
+            self.assertEqual(len(graph.nodes), 3)
+            return graph
+
+        graph = load_graph()
+
+        # Rebuild of my-software does not trigger rebuild of other packages.
+        build_requirements = graph.solve(
+            Package('my-software', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 1)
+        self.assertEqual(build_requirements[0].package.name, 'my-software')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+
+
+        # Rebuild of libone triggers rebuild of my-software because my-software
+        # build requires on one of libone subpackage.
+        build_requirements = graph.solve(
+            Package('libone', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 2)
+        self.assertEqual(build_requirements[0].package.name, 'libone')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+        self.assertEqual(build_requirements[1].package.name, 'my-software')
+        self.assertEqual(
+            build_requirements[1].reasons,
+            ["build requires on libone-devel"]
+        )
+
+        # Rebuild of libtwo triggers rebuild of libone and my-software because
+        # - libone build requires on one of libtwo subpackage
+        # - my-software build requires on one of libtwo subpackage and on one
+        #   of libone subpackage.
+        build_requirements = graph.solve(
+            Package('libtwo', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 3)
+        self.assertEqual(build_requirements[0].package.name, 'libtwo')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+        self.assertEqual(build_requirements[1].package.name, 'libone')
+        self.assertEqual(
+            build_requirements[1].reasons,
+            ["build requires on libtwo-devel"]
+        )
+        self.assertEqual(build_requirements[2].package.name, 'my-software')
+        self.assertCountEqual(
+            build_requirements[2].reasons,
+            [
+                "build requires on libone-devel",
+                "build requires on libtwo-devel"
+            ]
+        )
+
+        # Remove my-software package directory, redefine my-software package
+        # with dependencies in info.yaml and reload the graph.
+        shutil.rmtree(self.pkgdirs['my-software'])
+        self.make_pkg(
+            name='my-software',
+            build_requires=['libone-devel, libtwo-devel'],
+            metadata={
+                'depends': ['libtwo']
+            }
+        )
+        graph = load_graph()
+
+        # Rebuild of libone MUST NOT trigger rebuild of my-software anymore
+        # because my-software dependencies defined in info.yaml now overrides
+        # build requires in RPM spec file.
+        self.assertEqual(
+            len(
+                graph.solve(
+                    Package('libone', self.config, self.staff, self.modules)
+                )
+            ),
+            1
+        )
+
+    def test_multiple_packages_with_provides(self):
+        """ Test graph with multiple packages and dependencies on provides in RPM spec files """
+        # Define 2 packages without depends in info.yaml but with build requires
+        # on other subpackages provides.
+        self.make_pkg(
+            name='libone',
+            subpackages=[
+                SubPackage('libone-bin'),
+                SubPackage('libone-devel')
+            ]
+        )
+        self.make_pkg(
+            name='my-software',
+            build_requires=['libone-provide = 3'],
+        )
+
+        def load_graph():
+            graph = PackagesDependencyGraph.from_project(
+                self.config,
+                self.staff,
+                self.modules
+            )
+            self.assertEqual(len(graph.nodes), 2)
+            return graph
+
+        graph = load_graph()
+
+        # Rebuild of libone triggers rebuild of my-software because my-software
+        # build requires on one of libone subpackage provides.
+        build_requirements = graph.solve(
+            Package('libone', self.config, self.staff, self.modules)
+        )
+        self.assertEqual(len(build_requirements), 2)
+        self.assertEqual(build_requirements[0].package.name, 'libone')
+        self.assertEqual(build_requirements[0].reasons, ["User request"])
+        self.assertEqual(build_requirements[1].package.name, 'my-software')
+        self.assertEqual(
+            build_requirements[1].reasons,
+            ["build requires on libone-provide"]
+        )
+
+    def test_loop(self):
+        """ Test graph solve with dependency loop """
+        # Define 3 packages with a dependency loop.
+        self.make_pkg(
+            name='libone',
+            metadata={
+                'depends': 'libtwo'
+            }
+        )
+        self.make_pkg(
+            name='libtwo',
+            metadata={
+                'depends': 'libthree'
+            }
+        )
+        self.make_pkg(
+            name='libthree',
+            metadata={
+                'depends': 'libone'
+            }
+        )
+
+        # Load graph
+        graph = PackagesDependencyGraph.from_project(
+            self.config,
+            self.staff,
+            self.modules
+        )
+        self.assertEqual(len(graph.nodes), 3)
+
+        # For all three package, the resolution should return all three
+        # build requirements.
+        for package in ['libone', 'libtwo', 'libthree']:
+            self.assertEqual(
+                len(
+                    graph.solve(
+                        Package(package, self.config, self.staff, self.modules)
+                    )
+                ),
+                3
+            )

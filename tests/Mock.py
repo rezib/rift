ded0d8487a542a6c6c0a3db4e97722c3e861c0b1
Rémi Palancher <remi@rackslab.io>
2024-06-06 11:14:08 +0200

    Tests: add tests for Mock.init()
    Verify mock files creation and error reporting in case of mock command
    failure.
    
    Change-Id: I45e62d118d64a84cf8fab50cb6fee06fd5ce57f5

--- tests/Mock.py
+++ tests/Mock.py
@@ -2,17 +2,21 @@
 # Copyright (C) 2023 CEA
 #
 
+import os
 import getpass
-from TestUtils import make_temp_dir, RiftTestCase
+from unittest.mock import patch
+
+from TestUtils import make_temp_dir, RiftProjectTestCase
 from rift.Mock import Mock
 from rift.Repository import Repository
+from rift import RiftError
 
-class MockTest(RiftTestCase):
+class MockTest(RiftProjectTestCase):
     """
     Tests class for Mock
     """
 
-    def test_init(self):
+    def test_mock_object(self):
         """ Test Mock instanciation """
         mock = Mock(config=[], arch='x86_64', proj_vers=1.0)
         self.assertEqual(mock._mockname, "rift-x86_64-{}-1.0".format(getpass.getuser()))
@@ -44,3 +48,30 @@ class MockTest(RiftTestCase):
         self.assertEqual(repos_ctx['module_hotfixes'], True)
         self.assertEqual(repos_ctx['excludepkgs'], 'somepkg')
         self.assertEqual(repos_ctx['proxy'], 'myproxy')
+
+    @patch('rift.Mock.Popen')
+    def test_init(self, mock_popen):
+        """ Test Mock init creates all files required by mock """
+        # Emulate successful mock execution
+        mock_popen.return_value.returncode = 0
+        mock = Mock(config=self.config, arch='x86_64', proj_vers=1.0)
+        mock.init([])
+        self.assertTrue(
+            os.path.exists(os.path.join(mock._tmpdir.path, mock.MOCK_DEFAULT))
+        )
+        for filename in mock.MOCK_FILES:
+            self.assertTrue(
+                os.path.exists(os.path.join(mock._tmpdir.path, filename))
+            )
+        mock.clean()
+
+    @patch('rift.Mock.Popen')
+    def test_init_mock_failure(self, mock_popen):
+        """ Test Mock init raise error on mock command failure """
+        # Emulate mock execution failure
+        mock_popen.return_value.returncode = 1
+        mock_popen.return_value.communicate.return_value = ["output"]
+        mock = Mock(config=self.config, arch='x86_64', proj_vers=1.0)
+        with self.assertRaisesRegex(RiftError, "^output$"):
+            mock.init([])
+        mock.clean()

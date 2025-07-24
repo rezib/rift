3a3bd77b94b3acaf125f51fa5caf51501a428ab7
Rémi Palancher <remi@rackslab.io>
2025-07-24 14:23:07 +0200

    RPM: temporary C locale for date in RPM changelog
    To avoid RPM library complaining about bad date format due to days or
    months in non-english language, temporarily set C locale before
    generating the date string aiming to land in Spec changelog.

--- lib/rift/RPM.py
+++ lib/rift/RPM.py
@@ -40,6 +40,7 @@ import re
 import shutil
 from subprocess import Popen, PIPE, STDOUT, run, CalledProcessError
 import time
+import locale
 
 import rpm
 
@@ -364,7 +365,14 @@ class Spec():
         if bump:
             self.bump_release()
 
+        # Temporarily set basic C locale to generate date representation in
+        # changelog.
+        current_locale = locale.getlocale(locale.LC_TIME)
+        locale.setlocale(locale.LC_TIME, 'C')
         date = time.strftime("%a %b %d %Y", time.gmtime())
+        # Immediately restore previous locale.
+        locale.setlocale(locale.LC_TIME, current_locale)
+
         newchangelogentry = f"* {date} {userstring} - {self.evr}\n{comment}\n"
         chlg_match = None
         for i, _ in enumerate(self.lines):

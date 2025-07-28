76522358f972cc386d6cab10b39d785efc9872a4
Rémi Palancher <remi@rackslab.io>
2025-07-28 10:36:46 +0200

    Controller: make gerrit action format agnostic

--- lib/rift/package/_base.py
+++ lib/rift/package/_base.py
@@ -230,6 +230,10 @@ class Package():
         """Must be implemented in concrete children classes when supported."""
         raise NotImplementedError
 
+    def analyze(self, review, configdir):
+        """Must be implemented in concrete children classes when supported."""
+        raise NotImplementedError
+
 
 class ActionableArchPackage:
     """

d9e5b560268682ea9df3ae4f2a3fe1c7453b69bc
Rémi Palancher <remi@rackslab.io>
2025-08-27 16:24:11 +0200

    Tests: add dummy test script in generated package

--- tests/package/rpm.py
+++ tests/package/rpm.py
@@ -507,7 +507,7 @@ class ActionableArchPackageRPMTest(RiftProjectTestCase):
         self.setup_package()
         results = self.pkg.test()
         self.assertIsInstance(results, TestResults)
-        self.assertEqual(len(results), 1)
+        self.assertEqual(len(results), 2)
         self.assertEqual(results.global_result, True)
         # Check VM initialized (w/o extra repository)
         mock_vm.assert_called_once_with(self.config, 'x86_64', extra_repos=[])
@@ -612,7 +612,7 @@ class ActionableArchPackageRPMTest(RiftProjectTestCase):
         self.setup_package()
         results = self.pkg.test()
         self.assertIsInstance(results, TestResults)
-        self.assertEqual(len(results), 1)
+        self.assertEqual(len(results), 2)
         self.assertEqual(results.global_result, False)
 
     @patch('rift.package.rpm.time.sleep')
@@ -622,7 +622,7 @@ class ActionableArchPackageRPMTest(RiftProjectTestCase):
         # mock time.sleep() to avoid waiting sleep timeout when VM is stopped
         mock_vm_obj = mock_vm.return_value
         mock_vm_obj.running.return_value = False
-        self.setup_package()
+        self.setup_package(tests=[])
         results = self.pkg.test(noauto=True)
         # Check empty TestResults
         self.assertIsInstance(results, TestResults)

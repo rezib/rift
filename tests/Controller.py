0ff155dfa94aa5fe267356a407c00bc4a54ea980
Rémi Palancher <remi@rackslab.io>
2025-09-04 10:56:52 +0200

    TestResults: add package format in TestCase

--- tests/Controller.py
+++ tests/Controller.py
@@ -826,7 +826,8 @@ class ControllerProjectActionBuildTest(RiftProjectTestCase):
         mock_pkg_rpm_objs.for_arch.return_value = mock_act_arch_pkg_rpm
         # Make ActionableArchPackageRPM.test() return results with one failure.
         test_results = TestResults()
-        test_results.add_failure(TestCase('fake', 'pkg', 'x86_64'), 0, None, None)
+        test_results.add_failure(
+            TestCase('fake', 'pkg', 'x86_64', 'rpm'), 0, None, None)
         mock_act_arch_pkg_rpm.test.return_value = test_results
 
         # Run test on package and check main returns non-zero exit code
@@ -1080,7 +1081,8 @@ class ControllerProjectActionBuildTest(RiftProjectTestCase):
         mock_pkg_rpm_objs.for_arch.return_value = mock_act_arch_pkg_rpm
         # Make ActionableArchPackageRPM.test() return results with one failure.
         test_results = TestResults()
-        test_results.add_failure(TestCase('fake', 'pkg', 'x86_64'), 0, None, None)
+        test_results.add_failure(
+            TestCase('fake', 'pkg', 'x86_64', 'rpm'), 0, None, None)
         mock_act_arch_pkg_rpm.test.return_value = test_results
 
         # Run validate on package and check main returns non-zero exit code
@@ -1225,7 +1227,8 @@ class ControllerProjectActionBuildTest(RiftProjectTestCase):
         mock_pkg_rpm_objs.for_arch.return_value = mock_act_arch_pkg_rpm
         # Make ActionableArchPackageRPM.test() return results with one failure.
         test_results = TestResults()
-        test_results.add_failure(TestCase('fake', 'pkg', 'x86_64'), 0, None, None)
+        test_results.add_failure(
+            TestCase('fake', 'pkg', 'x86_64', 'rpm'), 0, None, None)
         mock_act_arch_pkg_rpm.test.return_value = test_results
 
         # Run validate on package and check main returns non-zero exit code

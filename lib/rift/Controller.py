e40df0bb033ab0d5504a36d7566a57d02abe75f0
Rémi Palancher <remi@rackslab.io>
2025-09-04 10:56:52 +0200

    TestResults: add package format in TestCase
    In preparation for multi-format support in Rift, test cases must display
    the format considered for the test in order to be able to distinguish
    the results for each of the formats supported by the packages being
    processed.

--- lib/rift/Controller.py
+++ lib/rift/Controller.py
@@ -440,7 +440,7 @@ def validate_pkgs(config, args, pkgs, arch):
 
     for pkg in pkgs:
         # Load package and report possible failure
-        case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch)
+        case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch, pkg.format)
         now = time.time()
         try:
             pkg.load()
@@ -473,7 +473,7 @@ def validate_pkgs(config, args, pkgs, arch):
 
         try:
             now = time.time()
-            case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch)
+            case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch, pkg.format)
             pkg_arch.build(sign=args.sign, staging=staging)
         except RiftError as ex:
             logging.error("Build failure: %s", str(ex))
@@ -594,7 +594,7 @@ def build_pkgs(args, pkgs, arch, staging):
 
     for pkg in pkgs:
         # Load package and report possible failure
-        case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch)
+        case = TestCase('build', pkg.name, _DEFAULT_VARIANT, arch, pkg.format)
         now = time.time()
         try:
             pkg.load()
@@ -718,7 +718,7 @@ def action_test(args, config):
                 # Create a dummy parse test case to report this error
                 # specifically. When parsing succeeds, this test case is not
                 # reported in test results.
-                case = TestCase("load", pkg.name, _DEFAULT_VARIANT, arch)
+                case = TestCase("load", pkg.name, _DEFAULT_VARIANT, arch, pkg.format)
                 logging.error("Unable to load package: %s", str(ex))
                 results.add_failure(case, time.time() - now, err=str(ex))
                 continue  # skip current package

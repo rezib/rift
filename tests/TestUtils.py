1767437e849ba329e9ffa0fda7a33dee11d4c76e
Rémi Palancher <remi@rackslab.io>
2025-09-18 15:26:12 +0200

    Test: marker to restrict to specific formats
    Similarly to local tests, it is now possible to add markers in test
    headers to restrict the test to one or multiple package formats.

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -156,7 +156,7 @@ Description for package {{ name }} variant %{variant}
 """
 
 SubPackage = namedtuple("SubPackage", ["name"])
-PackageTestDef = namedtuple("PackageTestDef", ["name", "local"])
+PackageTestDef = namedtuple("PackageTestDef", ["name", "local", "formats"])
 
 
 class RiftTestCase(unittest.TestCase):
@@ -396,7 +396,7 @@ class RiftProjectTestCase(RiftTestCase):
         # Add ./tests/0_test.sh by default
         if tests is None:
             tests = [
-                PackageTestDef(name='0_test.sh', local=False)
+                PackageTestDef(name='0_test.sh', local=False, formats=[])
             ]
         for test in tests:
             test_file = os.path.join(testsdir, test.name)
@@ -404,6 +404,8 @@ class RiftProjectTestCase(RiftTestCase):
                 fh.write('#!/bin/sh\n')
                 if test.local:
                     fh.write('# *** RIFT LOCAL ***\n')
+                for _format in test.formats:
+                    fh.write(f"# *** RIFT FORMAT {_format} ***\n")
                 fh.write('true')
 
     def clean_mock_environments(self):

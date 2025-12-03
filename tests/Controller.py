154d8e16c87d9c2300d24011e96db807796fee50
Rémi Palancher <remi@rackslab.io>
2025-12-03 09:21:20 +0100

    changelog: specific exc maintainer not in staff
    When running changelog action, raise a specific exception instead of a
    generic TypeError when maintainer provided in args cannot be found in
    project's staff.

--- tests/Controller.py
+++ tests/Controller.py
@@ -1193,7 +1193,9 @@ class ControllerProjectActionChangelogTest(RiftProjectTestCase):
     def test_action_changelog_unknown_maintainer(self):
         """changelog with unknown maintainer"""
         self.make_pkg()
-        with self.assertRaises(TypeError):
+        with self.assertRaisesRegex(
+            RiftError, "Unknown maintainer Fail, cannot be found in staff"
+        ):
             main(['changelog', 'pkg', '-c', 'basic change', '-t', 'Fail'])
 
 

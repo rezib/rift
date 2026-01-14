085bd62b2d3712fc5f3375b3a3840cef850b6c20
Rémi Palancher <remi@rackslab.io>
2026-01-14 11:21:47 +0100

    graph: make graph build format agnostic

--- tests/package/base.py
+++ tests/package/base.py
@@ -40,6 +40,22 @@ class PackageTest(RiftProjectTestCase):
         self.assertEqual(pkg.reason, 'Missing feature')
         self.assertEqual(pkg.origin, 'Vendor')
 
+    def test_subpackages(self):
+        """ Test Package subpackages (not implemented) """
+        pkgname = 'pkg'
+        pkg = Package(pkgname, self.config, self.staff, self.modules, 'rpm',
+                      f"{pkgname}.spec")
+        with self.assertRaises(NotImplementedError):
+            pkg.subpackages()
+
+    def test_build_requires(self):
+        """ Test Package build requires (not implemented) """
+        pkgname = 'pkg'
+        pkg = Package(pkgname, self.config, self.staff, self.modules, 'rpm',
+                      f"{pkgname}.spec")
+        with self.assertRaises(NotImplementedError):
+            pkg.build_requires()
+
     def test_add_changelog_entry(self):
         """ Test Package add changelog entry (not implemented) """
         pkgname = 'pkg'
